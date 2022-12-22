f8e1ec7babe192d4ecbda97a4ab6416f5857a719
daizhixia <daizhixia@mbp.attlocal.net>
2022-12-21 17:05:15 -0800

    fix

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,28 +1,22 @@
-import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class MyClass {
-    String key;
-    int count;
-    long timestamp;
-    TreeMap<String, MyClass> map = new TreeMap(
-            new Comparator<MyClass>() {
-                @Override
-                public int compare(MyClass o1, MyClass o2) {
-                    return (int) (o1.timestamp - o2.timestamp);
-                }
-            }
-    );
-    public MyClass(String key, int count, long timestamp){
-        this.key = key;
-        this.count = count;
-        this.timestamp = timestamp;
-    }
+    Map<String, TreeMap<Long, Integer>> map = new HashMap<>();
+
     public void add_mapping(String key, int count, long timestamp){
-        map.put(key, new MyClass(key, count, timestamp));
+        TreeMap<Long, Integer> treeMap = new TreeMap<>();
+        treeMap.putIfAbsent(timestamp, count);
+        map.put(key, treeMap);
     }
     public int get_feature_value(String key, long timestamp) {
-        return map.get(key).count;
+        return map.get(key).get(timestamp);
+    }
+
+    public static void main(String args []){
+        MyClass myClass = new MyClass();
+        myClass.add_mapping("key1", 2, System.currentTimeMillis());
     }
 
 }
